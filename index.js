92cef31159433da32edb8de024855dc1bb09a031
NorbertCiro <salazarbritonorberto@gmail.com>
2024-07-17 14:50:37 -0500

    Added .editorconfig for project settings and new paths

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require('express')
 const app = express()
 const port = 3000
- 
+
 app.get('/', ( _req, res ) => {
   res.send('Servidor iniciado en express')
 })
 
-app.listen(port, () => {
-  console.log( 'Mi port ' + port )
+app.get('/products', ( _req, res ) => {
+  res.json({
+    name: 'Xiaomi 5G lite 11',
+    price: 1000
+  })
+
 })
+
+app.listen(port, () => {
+  console.log( `El servidor esta corriendo en el puerto ${port}` )
+})
+
